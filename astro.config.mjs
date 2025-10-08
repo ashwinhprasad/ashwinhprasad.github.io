6b43e0e96c20585b7b959b433f07ccfd68512aa7
ashwinhprasad <ashwin-17068@ashwin-17068.csez.zohocorpin.com>
2025-10-09 02:28:39 +0530

    Deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@
 import { defineConfig } from 'astro/config';
 import pagefind from "astro-pagefind";
 
-// https://astro.build/config
+
 export default defineConfig({
+    site: 'https://ashwinhprasad.github.io/',
     integrations: [pagefind(
         
     )],
